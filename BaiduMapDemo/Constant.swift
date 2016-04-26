abf76d0a05ab5e2989b9060fcefeb926a44d366b
marquis <511680713@qq.com>
2016-04-26 10:58:39 +0800

    增加定位模式

--- BaiduMapDemo/Constant.swift
+++ BaiduMapDemo/Constant.swift
@@ -13,3 +13,5 @@ let rangeDelta = ["lat": 0.128325, "lng": 0.082045]
 let macaoCenter = ["lat": 22.170404, "lng": 113.575519]
 let documentPath = NSSearchPathForDirectoriesInDomains(NSSearchPathDirectory.DocumentDirectory, NSSearchPathDomainMask.UserDomainMask, true)
 let mapDataPath = documentPath.last!+"/vmp/aomentebiexingzheng_2911.dat"
+
+let locationModel = ["None": BMKUserTrackingModeNone, "ModeFollow": BMKUserTrackingModeFollow, "ModeFollowWithHeading": BMKUserTrackingModeFollowWithHeading]
